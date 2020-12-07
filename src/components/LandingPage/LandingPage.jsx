86c5bf3a335dcaf2ec00225dc3967b0126a7943d
nejmiyassine01 <nejmi.yassine01@gmail.com>
2020-12-07 23:57:21 +0100

    Landing Page Responsive

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -17,7 +17,7 @@ function LandingPage() {
                         agency
                     </h2>
                     <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
                     </p>
                     <button>
                         Learn More
